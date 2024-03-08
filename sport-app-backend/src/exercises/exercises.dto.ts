8eb337ef62944fd5959be04de43d1a643ce6ea0c
Чупинин Антон <chupinin@intervolga.ru>
2024-03-08 23:08:34 +0300

    [exercise] добавлены dto для упражнений

---
+++ sport-app-backend/src/exercises/exercises.dto.ts
@@ -0,0 +1,41 @@
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
+const nameMinLength = 3;
+const nameMaxLength = 256;
+const descriptionMinLength = 10;
+const descriptionMaxLength = 1024;
+
+export class CreateExerciseDto {
+  @IsNotEmpty()
+  @MinLength(nameMinLength)
+  @MaxLength(nameMaxLength)
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(descriptionMinLength)
+  @MaxLength(descriptionMaxLength)
+  description: string;
+
+  image?: string;
+  gif?: string;
+  video?: string;
+}
+
+export class updateExerciseDto {
+  
+  id: number;
+
+  @IsNotEmpty()
+  @MinLength(nameMinLength)
+  @MaxLength(nameMaxLength)
+  name: string;
+
+  @IsNotEmpty()
+  @MinLength(descriptionMinLength)
+  @MaxLength(descriptionMaxLength)
+  description: string;
+
+  image?: string;
+  gif?: string;
+  video?: string;
+}
