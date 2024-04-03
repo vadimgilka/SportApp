3c03b210b647059a55d96f2a1c5f4302b4d33c6f
Чупинин Антон <chupinin@intervolga.ru>
2024-04-04 01:07:28 +0300

    [train] добавлен trainModule

--- sport-app-backend/src/history/history.service.spec.ts
+++ sport-app-backend/src/history/history.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HistoryService } from './history.service';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'nestjs-prisma';
 
 describe('HistoryService', () => {
   let service: HistoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HistoryService],
+      providers: [HistoryService, PrismaService],
     }).compile();
 
     service = module.get<HistoryService>(HistoryService);
