9a608cdd1aa19b5587496b30beb4d2c91697ce33
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2024-05-25 01:19:51 +0300

    [fix] уведомления 2

--- sport-app-backend/src/reminds/reminds.service.ts
+++ sport-app-backend/src/reminds/reminds.service.ts
@@ -27,7 +27,7 @@ export class RemindsService {
     cursor?: Prisma.RemindWhereUniqueInput;
     where?: Prisma.RemindWhereInput;
     orderBy?: Prisma.RemindOrderByWithRelationInput;
-    include?: Prisma.RemindInclude
+    include?: Prisma.RemindInclude;
   }): Promise<Remind[]> {
     const { skip, take, cursor, where, orderBy } = params;
     return this.prisma.remind.findMany({
@@ -60,8 +60,10 @@ export class RemindsService {
 
     const time = data.time;
 
-    if(!time || time < 0 || time > 1440){
-      throw new BadRequestException("incorrect time value. Time must be between 0 and 1440");
+    if (!time || time < 0 || time > 1440) {
+      throw new BadRequestException(
+        'incorrect time value. Time must be between 0 and 1440',
+      );
     }
 
     return this.prisma.remind.create({
@@ -81,8 +83,10 @@ export class RemindsService {
 
     const time = data.time;
 
-    if(isNumber(time) && (time < 0 || time > 1440)){
-      throw new BadRequestException("incorrect time value. Time must be between 0 and 1440");
+    if (isNumber(time) && (time < 0 || time > 1440)) {
+      throw new BadRequestException(
+        'incorrect time value. Time must be between 0 and 1440',
+      );
     }
 
     return this.prisma.remind.update({
@@ -99,21 +103,20 @@ export class RemindsService {
 
   @Cron('*/1 * * * *')
   async sendReminder() {
-
-     const time = this.getTime();
-     const reminds = await this.reminds({
-      where : {time},
+    const time = this.getTime();
+    const reminds = await this.reminds({
+      where: { time },
       // include : {
       //   BiologicalAdditive : true
       // }
-     })
+    });
 
-     for(const remind of reminds){
-        logger.info(remind)
-        if(remind.token){
-            await this.fcm.sendMessage(this.remindToPayloadFCM(remind));
-        }
-     }
+    for (const remind of reminds) {
+      logger.info(remind);
+      if (remind.token) {
+        await this.fcm.sendMessage(this.remindToPayloadFCM(remind));
+      }
+    }
   }
 
   private getTime() {
@@ -121,26 +124,25 @@ export class RemindsService {
     const hours = currentTime.getUTCHours();
     const minutes = currentTime.getUTCMinutes();
 
-    console.log(hours + ":" + minutes)
+    console.log(hours + ':' + minutes);
 
     return hours * 60 + minutes;
   }
 
-  private remindToPayloadFCM(remind : Remind) : PayloadFCM{
-     const payload : PayloadFCM = {
-       token : remind.token,
-       data : {
-
-        notification : {
-          title : "время пить таблетки",
-          body : "твоя дозировка =  " + remind.measure,  
+  private remindToPayloadFCM(remind: Remind): PayloadFCM {
+    const payload: PayloadFCM = {
+      token: remind.token,
+      data: {
+        notification: {
+          title: 'время пить таблетки',
+          body: 'твоя дозировка =  ' + remind.measure,
+        },
+        id: String(remind.id),
+        time: String(remind.time),
+        measure: String(remind.measure),
       },
-         id : String(remind.id),
-         time : String(remind.time),
-         measure : remind.measure,
-       }
-     }
+    };
 
-     return payload;
+    return payload;
   }
 }
