00d5be3363727d20c7f267a023bfe6fcbd90688b
Chupinin Anton <82999563+vakhew1900@users.noreply.github.com>
2024-05-25 01:26:17 +0300

    [fix] уведомления 3

--- sport-app-backend/src/fcm-notification/fcm-notification.service.ts
+++ sport-app-backend/src/fcm-notification/fcm-notification.service.ts
@@ -14,6 +14,7 @@ import logger from 'src/prisma/logger';
 
    async sendMessage(payload : PayloadFCM){
       try {
+        logger.info(JSON.stringify(payload))
          await admin.messaging().send(payload);
       }
       catch(e){
